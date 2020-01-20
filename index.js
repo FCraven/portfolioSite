d04a5f98c9292611be3c05066642563748cb4400
FCraven <craven.francis@gmail.com>
2020-01-20 01:58:56 -0500

    navbar animated and looking decent across devices for now

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const menuButton = document.getElementById('menu-button');
+const menuButtonIcon = document.getElementById('menu-button-icon')
+const navbar = document.getElementById('nav-container');
 
  function navbarOpen() {
-   const fullMenu = document.getElementById('nav-container');
-   fullMenu.style.transform = 'rotateY(0deg)';
+   navbar.style.transform = 'rotateX(0deg)';
+   menuButton.classList.toggle('active')
+   menuButtonIcon.classList.remove('fas', 'fa-bars', 'fa-2x')
+   menuButtonIcon.classList.add('fas', 'fa-arrow-right', 'fa-2x')
+   console.log(`navbar open`)
+ }
 
+ function navbarClose() {
+   console.log(`navbar close`)
+  navbar.style.transform = 'rotateX(90deg)'
+  menuButton.classList.toggle('active')
+  menuButtonIcon.classList.remove('fas', 'fa-arrow-right', 'fa-2x')
+  menuButtonIcon.classList.add('fas', 'fa-bars', 'fa-2x')
  }
 
 //LISTENERS
- menuButton.addEventListener('click', navbarOpen);
+menuButton.addEventListener('click', function(){
+  const list = menuButton.classList
+  if(list.contains('active')) {
+    navbarClose()
+  } else {
+    navbarOpen()
+  }
+});
